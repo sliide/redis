bc3c70a4e5ec48cad7d99fb1a47b6611811db927
andrefsp <andrefsp@gmail.com>
2016-05-11 13:08:19 +0100

    Add redis pool client

---
+++ pool.go
@@ -0,0 +1,110 @@
+package redis
+
+import (
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+var pool *redis.Pool = nil
+
+func newPool(server string) *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", server)
+			if err != nil {
+				return nil, err
+			}
+			return c, nil
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
+
+func Init(redisURL string) (err error) {
+	pool = newPool(redisURL)
+	return nil
+}
+
+func Close() {
+	pool.Close()
+}
+
+func Get(key string) (val string, err error) {
+	c := pool.Get()
+	defer c.Close()
+
+	val, err = redis.String(c.Do("GET", key))
+	return
+}
+
+func Set(key string, value interface{}) (err error) {
+	c := pool.Get()
+	defer c.Close()
+
+	_, err = c.Do("SET", key, value)
+
+	return
+}
+
+func LPush(key string, value string) (err error) {
+	c := pool.Get()
+	defer c.Close()
+
+	_, err = c.Do("LPUSH", key, value)
+	return
+}
+
+func RPush(key string, value string) (err error) {
+	c := pool.Get()
+	defer c.Close()
+
+	_, err = c.Do("RPUSH", key, value)
+	return
+}
+
+func LRange(key string) (vals []string, err error) {
+	c := pool.Get()
+	defer c.Close()
+
+	vals, err = redis.Strings(c.Do("LRANGE", key, 0, -1))
+	return vals, err
+}
+
+func Pop(key string) (val string, err error) {
+	c := pool.Get()
+	defer c.Close()
+
+	val, err = redis.String(c.Do("LPOP", key))
+	return
+}
+
+func Incr(key string) (err error) {
+	c := pool.Get()
+	defer c.Close()
+
+	_, err = c.Do("INCR", key)
+	return
+}
+
+func IncrBy(key string, inc interface{}) (err error) {
+	c := pool.Get()
+	defer c.Close()
+
+	_, err = c.Do("INCRBY", key, inc)
+	return
+}
+
+func Expire(key string, seconds int) (err error) {
+	c := pool.Get()
+	defer c.Close()
+
+	_, err = c.Do("EXPIRE", key, seconds)
+
+	return err
+}
---
+++ pool_test.go
@@ -0,0 +1,149 @@
+package redis_test
+
+import (
+	"log"
+	"strconv"
+	"testing"
+
+	"github.com/sliide/redis"
+
+	"math/rand"
+	"time"
+
+	. "gopkg.in/check.v1"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+type RedisTestSuite struct {}
+
+var _ = Suite(
+	&RedisTestSuite{},
+)
+
+func (s *RedisTestSuite) SetUpSuite(c *C) {
+	redis.Init("localhost:6379")
+}
+
+func (s *RedisTestSuite) TearDownSuite(c *C) {
+	redis.Close()
+}
+
+func (s *RedisTestSuite) TestIncrBy(c *C) {
+	key := randSeq(32)
+
+	c.Assert(redis.Set(key, 1), IsNil)
+	c.Assert(redis.IncrBy(key, 10), IsNil)
+
+	val, err := redis.Get(key)
+
+	if err != nil {
+		log.Println(err)
+		c.Fail()
+	}
+
+	c.Assert(val, Equals, strconv.Itoa(11))
+}
+
+func (s *RedisTestSuite) TestExpire(c *C) {
+	key := randSeq(32)
+
+	c.Assert(redis.Set(key, "1"), IsNil)
+
+	val, err := redis.Get(key)
+	c.Assert(err, IsNil)
+	c.Assert(val, Equals, "1")
+
+	c.Assert(redis.Expire(key, 1), IsNil)
+	time.Sleep(2 * time.Second)
+
+	val, err = redis.Get(key)
+	c.Assert(err, Not(IsNil))
+	c.Assert(val, Equals, "")
+}
+
+func (s *RedisTestSuite) TestRPush(c *C) {
+	key := randSeq(32)
+
+	for i := 0; i < 2; i++ {
+		err := redis.RPush(key, strconv.Itoa(i))
+		c.Assert(err, Equals, nil)
+	}
+
+	vals, err := redis.LRange(key)
+	c.Assert(err, IsNil)
+	c.Assert(vals, DeepEquals, []string{"0", "1"})
+}
+
+func (s *RedisTestSuite) TestRedis(c *C) {
+
+	key := randSeq(32)
+	pop := randSeq(32)
+	val := randSeq(32)
+	val2 := randSeq(32)
+	val3 := randSeq(32)
+
+	key2 := randSeq(32)
+
+	v, err := redis.Get(key)
+	c.Assert(err, Not(Equals), nil)
+
+	err = redis.Set(key, val)
+	c.Assert(err, Equals, nil)
+
+	v, err = redis.Get(key)
+	c.Assert(err, Equals, nil)
+	c.Assert(v, Equals, val)
+
+	v, err = redis.Pop(pop)
+	c.Assert(err, Not(Equals), nil)
+
+	err = redis.LPush(pop, val)
+	c.Assert(err, Equals, nil)
+
+	err = redis.LPush(pop, val2)
+	c.Assert(err, Equals, nil)
+
+	err = redis.LPush(pop, val3)
+	c.Assert(err, Equals, nil)
+
+	v, err = redis.Pop(pop)
+	c.Assert(err, Equals, nil)
+	c.Assert(v, Equals, val3)
+
+	v, err = redis.Pop(pop)
+	c.Assert(err, Equals, nil)
+	c.Assert(v, Equals, val2)
+
+	v, err = redis.Pop(pop)
+	c.Assert(err, Equals, nil)
+	c.Assert(v, Equals, val)
+
+	err = redis.Set(key2, "2")
+	c.Assert(err, Equals, nil)
+
+	err = redis.Incr(key2)
+	c.Assert(err, Equals, nil)
+
+	err = redis.Incr(key2)
+	c.Assert(err, Equals, nil)
+
+	err = redis.Incr(key2)
+	c.Assert(err, Equals, nil)
+
+	v, err = redis.Get(key2)
+	c.Assert(err, Equals, nil)
+	c.Assert(v, Equals, "5")
+}
+
+// TODO: move it somewhere so we don't copy this everywhere
+func randSeq(n int) string {
+	rand.Seed(time.Now().UnixNano())
+	var letters = []rune("0123456789")
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
